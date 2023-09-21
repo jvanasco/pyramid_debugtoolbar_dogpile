70d85e97f05e5bb17e26f2d62cbea0c11499ceea
jonathan vanasco <jonathan@2xlp.com>
2023-09-20 20:55:24 -0400

    v0.3.0 pre-release work

--- src/pyramid_debugtoolbar_dogpile/__init__.py
+++ src/pyramid_debugtoolbar_dogpile/__init__.py
@@ -1,52 +1,61 @@
-from __future__ import print_function
-
 # stdlib
 from collections import namedtuple
 import logging
 import time
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Mapping
+from typing import Optional
+from typing import Sequence
+from typing import TYPE_CHECKING
+from typing import Union
 
-# pyramid
-from pyramid.threadlocal import get_current_request
-
-# dogpile
-from dogpile.cache.api import NO_VALUE, CachedValue
+# pypi
+from dogpile.cache.api import BackendFormatted
+from dogpile.cache.api import BackendSetType
+from dogpile.cache.api import CacheBackend
+from dogpile.cache.api import CachedValue
+from dogpile.cache.api import KeyType
+from dogpile.cache.api import NO_VALUE
 from dogpile.cache.proxy import ProxyBackend
+from pyramid.threadlocal import get_current_request
 
 # local
 from .panels.pyramid_dogpile import DogpileDebugPanel
 
+if TYPE_CHECKING:
+    from pyramid.config import Configurator
+    from pyramid.request import Request
 
-__VERSION__ = "0.2.4"
+
+__VERSION__ = "0.3.0"
 
 
 # ==============================================================================
 
 
 LoggedEvent = namedtuple("LoggedEvent", ["key", "value", "size"])
+ApiCall = namedtuple("ApiCall", ["api_call", "duration", "db", "data"])
 
 
 # ==============================================================================
 
 
-def includeme(config):
+def includeme(config: "Configurator"):
+    logging.debug("includeme")
     config.add_debugtoolbar_panel(DogpileDebugPanel)
     # if 'mako.directories' not in config.registry.settings:
     #    config.registry.settings['mako.directories'] = []
 
-    # custom property || app_domain / DomainObject
-    config.add_request_method(
-        "pyramid_debugtoolbar_dogpile.setup_dogpile_logging",
-        "dogpile_logging",
-        reify=True,
-    )
 
-
-def setup_dogpile_logging(request):
+def setup_dogpile_logging(request: "Request") -> Dict:
     """
     calls_raw =  ordered list of calls
     """
     logging.debug("initializing setup_dogpile_logging")
-    return {"api_calls": []}  # (api_call, duration, db, (k + result))
+    rval: Dict[str, List] = {"api_calls": []}  # (api_call, duration, db, (k + result))
+    return rval
 
 
 class LoggingProxy(ProxyBackend):
@@ -55,22 +64,37 @@ class LoggingProxy(ProxyBackend):
     It times the performance of the backend and logs if we see cache hits or misses
     """
 
-    _connection_kwargs = None
-    _db = None
+    # None - unset
+    # Dict - values
+    # False - incompatible backend
+    _connection_kwargs: Optional[Union[bool, Dict]] = None
+    _db: Optional[Any] = None
 
     def _derive_db(self):
         """
         this may not have a client on __init__, so this is deferred to
         a @property descriptor
         """
-        client = self
+        client: Union[CacheBackend, ProxyBackend] = self
         while hasattr(client, "proxied"):
+            # ProxyBackend
             client = client.proxied
-        if hasattr(client, "client"):
-            client = client.client
-            self._connection_kwargs = client.connection_pool.connection_kwargs
-            self._db = self._connection_kwargs["db"]
-        else:
+        # client is now CacheBackend
+        #
+        # run this in a try/except block
+        # if we fail, or don't have `client`, use the Except block for fallbacks
+        try:
+            if hasattr(client, "client"):
+                # example: redis
+                client = client.client
+                _connection_kwargs: Dict = (
+                    client.connection_pool.connection_kwargs  # type: ignore[union-attr]
+                )
+                self._connection_kwargs = _connection_kwargs
+                self._db = _connection_kwargs["db"]
+            else:
+                raise ValueError()
+        except Exception:
             self._connection_kwargs = False
             self._db = "n/a"
 
@@ -80,41 +104,43 @@ class LoggingProxy(ProxyBackend):
             self._derive_db()
         return self._db
 
-    def set(self, key, value):
+    def set(self, key: KeyType, value: BackendSetType) -> None:
         _s = time.time()
         self.proxied.set(key, value)
         _f = time.time()
         _d = _f - _s
 
         r = get_current_request()
-        if r and hasattr(r, "dogpile_logging"):
-            r.dogpile_logging["api_calls"].append(
-                ("set", _d, self.db, [LoggedEvent(key, None, None)])
+        if r and hasattr(r, "_dogpile_logging"):
+            r._dogpile_logging["api_calls"].append(
+                ApiCall("set", _d, self.db, [LoggedEvent(key, None, None)])
             )
 
-    def set_multi(self, mapping):
+    def set_multi(self, mapping: Mapping[KeyType, BackendSetType]) -> None:
         _s = time.time()
         self.proxied.set_multi(mapping)
         _f = time.time()
         _d = _f - _s
 
         r = get_current_request()
-        if r and hasattr(r, "dogpile_logging"):
+        if r and hasattr(r, "_dogpile_logging"):
             _kvs = [LoggedEvent(k, None, None) for k in mapping.keys()]
-            r.dogpile_logging["api_calls"].append(("set_multi", _d, self.db, _kvs))
+            r._dogpile_logging["api_calls"].append(
+                ApiCall("set_multi", _d, self.db, _kvs)
+            )
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    def get(self, key):
+    def get(self, key: KeyType) -> BackendFormatted:
         _s = time.time()
         v = self.proxied.get(key)
         _f = time.time()
         _d = _f - _s
 
         r = get_current_request()
-        if r and hasattr(r, "dogpile_logging"):
-            r.dogpile_logging["api_calls"].append(
-                (
+        if r and hasattr(r, "_dogpile_logging"):
+            r._dogpile_logging["api_calls"].append(
+                ApiCall(
                     "get",
                     _d,
                     self.db,
@@ -131,15 +157,15 @@ class LoggingProxy(ProxyBackend):
             )
         return v
 
-    def get_multi(self, keys):
+    def get_multi(self, keys: Sequence[KeyType]) -> Sequence[BackendFormatted]:
         _s = time.time()
         vs = self.proxied.get_multi(keys)
         _f = time.time()
         _d = _f - _s
         r = get_current_request()
-        if r and hasattr(r, "dogpile_logging"):
+        if r and hasattr(r, "_dogpile_logging"):
             _results = []
-            for (idx, k) in enumerate(keys):
+            for idx, k in enumerate(keys):
                 v = vs[idx]
                 _results.append(
                     LoggedEvent(
@@ -150,29 +176,33 @@ class LoggingProxy(ProxyBackend):
                         else None,
                     )
                 )
-            r.dogpile_logging["api_calls"].append(("get_multi", _d, self.db, _results))
+            r._dogpile_logging["api_calls"].append(
+                ApiCall("get_multi", _d, self.db, _results)
+            )
         return vs
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    def delete(self, key):
+    def delete(self, key: KeyType) -> None:
         _s = time.time()
         self.proxied.delete(key)
         _f = time.time()
         _d = _f - _s
         r = get_current_request()
-        if r and hasattr(r, "dogpile_logging"):
-            r.dogpile_logging["api_calls"].append(
-                ("delete", _d, self.db, [LoggedEvent(key, None, None)])
+        if r and hasattr(r, "_dogpile_logging"):
+            r._dogpile_logging["api_calls"].append(
+                ApiCall("delete", _d, self.db, [LoggedEvent(key, None, None)])
             )
 
-    def delete_multi(self, keys):
+    def delete_multi(self, keys: Sequence[KeyType]) -> None:
         _s = time.time()
         self.proxied.delete_multi(keys)
         _f = time.time()
         _d = _f - _s
 
         r = get_current_request()
-        if r and hasattr(r, "dogpile_logging"):
+        if r and hasattr(r, "_dogpile_logging"):
             _kvs = [LoggedEvent(k, None, None) for k in keys]
-            r.dogpile_logging["api_calls"].append(("delete_multi", self.db, _d, _kvs))
+            r._dogpile_logging["api_calls"].append(
+                ApiCall("delete_multi", self.db, _d, _kvs)
+            )
--- src/pyramid_debugtoolbar_dogpile/panels/pyramid_dogpile.py
+++ src/pyramid_debugtoolbar_dogpile/panels/pyramid_dogpile.py
@@ -1,15 +1,28 @@
 # stdlib
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import TYPE_CHECKING
 
 # pyramid_debugtoolbar
 from pyramid_debugtoolbar.panels import DebugPanel
-from pyramid_debugtoolbar.utils import STATIC_PATH
 from pyramid_debugtoolbar.utils import ROOT_ROUTE_NAME
+from pyramid_debugtoolbar.utils import STATIC_PATH
 
+if TYPE_CHECKING:
+    from pyramid.request import Request
 
 # ==============================================================================
 
 
-dogpile_api_calls = ("get", "get_multi", "set", "set_multi", "delete", "delete_multi")
+dogpile_api_calls = (
+    "get",
+    "get_multi",
+    "set",
+    "set_multi",
+    "delete",
+    "delete_multi",
+)
 
 
 class DogpileDebugPanel(DebugPanel):
@@ -19,30 +32,31 @@ class DogpileDebugPanel(DebugPanel):
     template = "pyramid_debugtoolbar_dogpile.panels:templates/pyramid_dogpile.dbtmako"
     title = "Dogpile"
     nav_title = "Dogpile"
+    logs: Optional[List]
 
-    def __init__(self, request):
-        self.logs = (
-            request.dogpile_logging["api_calls"]
-            if hasattr(request, "dogpile_logging")
-            else None
-        )
+    def __init__(self, request: "Request"):
+        # set this attribute
+        _dogpile_logging: Dict = {"api_calls": []}
+        request._dogpile_logging = _dogpile_logging
+        self.logs = _dogpile_logging["api_calls"]
 
     @property
-    def has_content(self):
+    def has_content(self) -> bool:
         if self.logs:
             return True
         else:
             return False
 
     @property
-    def nav_subtitle(self):
+    def nav_subtitle(self) -> Optional[str]:
         if self.logs:
             return "%d" % (len(self.logs))
+        return None
 
-    def render_content(self, request):
+    def render_content(self, request: "Request") -> str:
         if not self.logs:
             return "No logs in request."
-        stats = {}
+        stats: Dict[str, Dict[str, int]] = {}
         for api_call in dogpile_api_calls:
             stats[api_call] = {
                 "hit": 0,
@@ -59,7 +73,7 @@ class DogpileDebugPanel(DebugPanel):
             # dk - dogpile key
             # dr - dogpile result
             if r[0] == "get_multi":
-                for (dk, dr, dsize) in r[3]:
+                for dk, dr, dsize in r[3]:
                     if dr is True:
                         stats[r[0]]["fractional-hit"] += 1
                     elif dr is False:
@@ -67,7 +81,7 @@ class DogpileDebugPanel(DebugPanel):
                     if dsize is not None:
                         stats[r[0]]["size"] += dsize
             else:
-                for (dk, dr, dsize) in r[3]:
+                for dk, dr, dsize in r[3]:
                     if dr is True:
                         stats[r[0]]["hit"] += 1
                     elif dr is False:
@@ -80,8 +94,9 @@ class DogpileDebugPanel(DebugPanel):
         self.data = {"logs": self.logs, "stats": stats, "has_size": bool(total_size)}
         return super(DogpileDebugPanel, self).render_content(request)
 
-    def render_vars(self, request):
-        return {
+    def render_vars(self, request: "Request") -> Dict:
+        r: Dict = {
             "static_path": request.static_url(STATIC_PATH),
             "root_path": request.route_url(ROOT_ROUTE_NAME),
         }
+        return r
